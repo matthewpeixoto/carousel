932e9430dafeadfbc64a6f0db9f4904c03abcf1b
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-02-03 11:48:30 -0500

    Add banner and content

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$('#bee-movie').on('click', function () {
+  $('.banner-image').css("background-image", "url(images/bee-movie.jpg)");
+  $('.banner-title').html("Bee Movie");
+  $('.banner-desc').html("Barry B. Benson, a bee just graduated from college, is disillusioned at his lone career choice: making honey. On a special trip outside the hive, Barry's life is saved by Vanessa, a florist in New York City.");
+});
+
+$('#dunkirk').on('click', function () {
+  $('.banner-image').css("background-image", "url(images/dunkirk.jpg)");
+  $('.banner-title').html("Dunkirk");
+  $('.banner-desc').html("Allied soldiers from Belgium, the British Empire and France are surrounded by the German Army, and evacuated during a fierce battle in World War II.");
+});
+
+
+$('#wonder-woman').on('click', function () {
+  $('.banner-image').css("background-image", "url(images/wonder-woman.jpg)");
+  $('.banner-title').html("Wonder Woman");
+  $('.banner-desc').html("When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.");
+});
