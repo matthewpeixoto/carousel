9d3e8c12b9cc05952c6f4052061f03d0aa46c895
Matthew Peixoto <matthewpeixoto@gmail.com>
2018-02-03 17:24:56 -0500

    Add play icon on hover

--- js/main.js
+++ js/main.js
@@ -1,39 +1,45 @@
-$('#bee-movie').on('click', function () {
+$('#bee-movie').on('mousmouseover', function () {
   $('.banner-image').css("background-image", "url(images/banner/bee-movie.jpg)");
   $('.banner-title').html("Bee Movie");
   $('.banner-desc').html("Barry B. Benson, a bee just graduated from college, is disillusioned at his lone career choice: making honey. On a special trip outside the hive, Barry's life is saved by Vanessa, a florist in New York City.");
 });
 
-$('#dunkirk').on('click', function () {
+$('#dunkirk').on('mouseover', function () {
   $('.banner-image').css("background-image", "url(images/banner/dunkirk.jpg)");
   $('.banner-title').html("Dunkirk");
   $('.banner-desc').html("Allied soldiers from Belgium, the British Empire and France are surrounded by the German Army, and evacuated during a fierce battle in World War II.");
+  $('.banner-content-dunkirk').addClass("reveal");
 });
 
 
-$('#wonder-woman').on('click', function () {
+$('#wonder-woman').on('mouseover', function () {
   $('.banner-image').css("background-image", "url(images/banner/wonder-woman.jpg)");
   $('.banner-title').html("Wonder Woman");
   $('.banner-desc').html("When a pilot crashes and tells of conflict in the outside world, Diana, an Amazonian warrior in training, leaves home to fight a war, discovering her full powers and true destiny.");
 });
 
-$('#blade-runner').on('click', function () {
+$('#blade-runner').on('mouseover', function () {
   $('.banner-image').css("background-image", "url(images/banner/blade-runner.jpg)");
   $('.banner-title').html("Blade Runner 2049");
   $('.banner-desc').html("A young blade runner's discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who's been missing for thirty years.");
 });
 
-$('#shrek-2').on('click', function () {
+$('#shrek-2').on('mouseover', function () {
   $('.banner-image').css("background-image", "url(images/banner/shrek-2.jpg)");
   $('.banner-title').html("Shrek 2");
   $('.banner-desc').html("Princess Fiona's parents invite her and Shrek to dinner to celebrate her marriage. If only they knew the newlyweds were both ogres.");
 });
 
-$('#baby-driver').on('click', function () {
+$('#baby-driver').on('mouseover', function () {
   $('.banner-image').css("background-image", "url(images/banner/baby-driver.jpg)");
   $('.banner-title').html("Baby Driver");
   $('.banner-desc').html("After being coerced into working for a crime boss, a young getaway driver finds himself taking part in a heist doomed to fail.");
 });
 
+$('#planet-of-the-apes').on('mouseover', function () {
+  $('.banner-image').css("background-image", "url(images/banner/planet-of-the-apes.jpg)");
+  $('.banner-title').html("War for the Planet of the Apes");
+  $('.banner-desc').html("After the apes suffer unimaginable losses, Caesar wrestles with his darker instincts and begins his own mythic quest to avenge his kind.");
+});
 
 
